14e6de3570a40073098f543d44e73071c1029793
Arnau <arnauescapa@gmail.com>
2018-04-09 18:38:58 +0200

    petits canvis

--- Task2/proves/js/pieV3.js
+++ Task2/proves/js/pieV3.js
@@ -64,7 +64,7 @@ var dades = [
 var w = 200;
 var h = 200;
 var r = h/2;
-var aColor = ["#ff0000","#a05c56","#961919","#e98125","#64a61f","#2383c1","#d8d239"]
+var aColor = ["#ff0000","#984ea3","#a65628","#e98125","#64a61f","#2383c1","#d8d239"]
 
 
 var vis = d3.select('#chart').append("svg:svg").data([dades]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
@@ -96,7 +96,7 @@ arcs.append("svg:text")
     // .attr("text-anchor", "middle")
     // .text( function(d, i) {return Math.round(dades[i].value/total*100) + '%';});
 
-vis.append("text").attr("id","pie_centre").text(pais_seleccio).style("font-size", 12).attr("transform","translate(-20,0)");
+vis.append("text").attr("id","pie_centre").text(pais_seleccio).style("font-size", 13).attr("transform","translate(-20,0)");
 function mousein(d){
   console.log('adeeu');
 
