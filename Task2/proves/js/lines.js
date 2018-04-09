6a20f398f02f4f03b97efbf0caaf9e43d1553559
mcardus <mcardus.g@gmail.com>
2018-04-09 03:53:07 +0200

    country selection implemented

--- Task2/proves/js/lines.js
+++ Task2/proves/js/lines.js
@@ -1,14 +1,23 @@
+function plot_lines(){
+
 d3.json("revenue.json", function(data) {
 
+  sub_data = []
+  for (i in data) {
+    if (data[i].name.indexOf(current_country) != -1) {
+      sub_data.push(data[i])
+    }
+  }
+  console.log(sub_data)
   var ordinalScale = d3.scale.ordinal()
           .domain([2004,2005,2006,2007])
           .range(['a', 'b', 'c', 'd', 'e']);
-  sub_data = data[0]
   lc = new LineChart({
     parent: '#multiple',
-    all_series: [sub_data],
+    all_series: sub_data,
     x_axis_text: "year",
     y_axis_text: "euros (€)"
   });
   lc.plot();
 })
+}
