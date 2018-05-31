183d40b4cc0f0cb2085651c9a2cabb2bc7756e75
enric <enricsarle@gmail.com>
2018-06-01 01:40:46 +0200

    ara es canvien el logo i les copes guanyades

---
+++ Task4/data/prova/js/change_logo.js
@@ -0,0 +1,5 @@
+function changeImage(current_franch) {
+    var ruta = ('../logos/'+current_franch+'.gif');
+    console.log(ruta);
+    document.getElementById("logo").src=ruta;
+}
--- Task4/data/prova/js/lines.js
+++ Task4/data/prova/js/lines.js
@@ -5,8 +5,8 @@ d3.json("home_away.json", function(data) {
   for (i in data) {
     if (data[i].name.indexOf(current_franch) != -1) {
       sub_data.push(data[i])
-    }
-    console.log(data[i]);
+    };
+    // console.log(data[i]);
   }
   lc = new LineChart({
     parent: '#multiple',
